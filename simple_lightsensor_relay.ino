24955eb0e22c3a9bf4e9be241ed3753a584f29f0
boerduino <47588101+boerduino@users.noreply.github.com>
2019-07-09 08:33:03 -0700

    Add files via upload

--- simple_lightsensor_relay.ino
+++ simple_lightsensor_relay.ino
@@ -8,14 +8,12 @@ int pin_digital_relay = 2;
 void setup() {
   pinMode(A2, INPUT); /* Input from analog photoresistor */
   pinMode(2, OUTPUT); /* Output to digital relay */
-  Serial.begin(9600);
 }
 
 void loop() {
   if(millis() - current_time > delay_time) {
     current_time = millis();
     io_photoresistor = analogRead(pin_analog_photoresistor);
-    Serial.println(io_photoresistor, DEC);
   }
   
   if (io_photoresistor > 400) {
