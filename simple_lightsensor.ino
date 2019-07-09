b6ae80993daa59aac4d31c71a410621b8c334ab3
boerduino <47588101+boerduino@users.noreply.github.com>
2019-07-09 06:36:30 -0700

    Add files via upload

--- simple_lightsensor.ino
+++ simple_lightsensor.ino
@@ -1,6 +1,6 @@
 int pin_photoresistor = 2;
 int io_photoresistor = 0;
-int delay_time = 5000;
+int delay_time = 1000;
 unsigned long current_time = 0;
 
 void setup() {
@@ -10,22 +10,9 @@ void setup() {
 }
 
 void loop() {
-  
-  
   if(millis() - current_time > delay_time) {
     current_time = millis();
     io_photoresistor = analogRead(pin_photoresistor);
-    
-    if (io_photoresistor > 400) { /* higher value (up to 1023) indicates lower light, lower value (down to 0) indicates more light */
-      Serial.println(io_photoresistor, DEC);
-    }
+    Serial.println(io_photoresistor, DEC);
   }
 }
-/*
-
-
-    if (io_photoresistor > 400) { /* higher value (up to 1023) indicates lower light, lower value (down to 0) indicates more light */
-    /* do something here */
-    }
-    
-    */
